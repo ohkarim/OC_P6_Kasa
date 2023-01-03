3c44c2cc7c9fda9e942313a79d19b6da194a054a
OHKarim <karim.ouldhammou@gmail.com>
2023-01-03 15:50:48 +0100

    NavLinks active style applied

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,34 @@
 import { Link, NavLink } from "react-router-dom";
 
-//TODO: for NavLink, make them "active" with underline, maybe with ternary 
-
 export default function NavBar() {
+    // Style to apply to active NavLinks
+    let activeStyle = {
+        textDecoration: "underline",
+      };
+
     return (
         <nav className="nav-bar">
             <li className="nav-bar--title">
-                <NavLink to="/" className="nav-bar--link">Accueil</NavLink>
+                <NavLink 
+                    to="/" 
+                    className="nav-bar--link"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                >
+                    Accueil
+                </NavLink>
             </li>
             <li className="nav-bar--title">
-                <NavLink to="/about" className="nav-bar--link">À Propos</NavLink>
+                <NavLink 
+                    to="/about" 
+                    className="nav-bar--link"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                >
+                    À Propos
+                </NavLink>
             </li>
         </nav>
     );
